a7247ea2ebf3470ef0d21a05f5de66a0559c7d96
Ulrik Damm <ulrikdamm@me.com>
2013-03-21 16:06:57 +0100

    .gitignore update

---
+++ html/search/defines_63.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['current_5fimplementation',['CURRENT_IMPLEMENTATION',['../cc__list_8c.html#af0392cac90907940839d54396beb091d',1,'CURRENT_IMPLEMENTATION():&#160;cc_list.c'],['../cc__set_8c.html#af0392cac90907940839d54396beb091d',1,'CURRENT_IMPLEMENTATION():&#160;cc_set.c']]]
+];
